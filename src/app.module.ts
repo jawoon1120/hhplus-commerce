94facbf8dd0f28b7476a13b235163e0f81cacae8
jeong <jw.jeong@tilda.co.kr>
2025-01-03 00:12:40 +0900

    주문 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CouponModule } from './modules/coupon/coupon.module';
 import { BalanceModule } from './balance/balance.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [CouponModule, BalanceModule, ProductModule],
+  imports: [CouponModule, BalanceModule, ProductModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
