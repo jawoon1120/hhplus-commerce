79850961243ae94c6416a8e5446e4791db947864
jeong <jw.jeong@tilda.co.kr>
2025-01-03 01:12:18 +0900

    :wrench: swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('항해 이커머스')
+    .setDescription('API 문서')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
