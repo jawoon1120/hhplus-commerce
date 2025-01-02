faf8fde3bbe216594d97eee6c2708bdc9e4f46f3
jeong <jw.jeong@tilda.co.kr>
2025-01-03 00:11:23 +0900

    :tada: 상품 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CouponModule } from './modules/coupon/coupon.module';
 import { BalanceModule } from './balance/balance.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CouponModule, BalanceModule],
+  imports: [CouponModule, BalanceModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
