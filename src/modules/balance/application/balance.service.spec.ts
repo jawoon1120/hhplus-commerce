d6af0556a262cca6a172df7820c32cfe0b9c20cb
jeong <jw.jeong@tilda.co.kr>
2025-01-09 01:36:41 +0900

    test code 수정

--- src/modules/balance/application/balance.service.spec.ts
+++ src/modules/balance/application/balance.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BalanceService } from './balance.service';
+import { IBalanceRepository } from '../domain/balance-repository.interface';
+import { CustomerService } from '../../customer/application/customer.service';
 
 describe('BalanceService', () => {
   let service: BalanceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BalanceService],
+      providers: [
+        BalanceService,
+        {
+          provide: IBalanceRepository,
+          useValue: {
+            getBalanceByUserId: jest.fn(),
+            chargeBalance: jest.fn(),
+            withdrawBalance: jest.fn(),
+          },
+        },
+        {
+          provide: CustomerService,
+          useValue: {
+            getCustomerById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<BalanceService>(BalanceService);
