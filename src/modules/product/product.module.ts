b14d4be0d2dae38cd5e70289152a85de65646cf0
jeong <jw.jeong@tilda.co.kr>
2025-01-09 01:10:35 +0900

    :sparkles: product module

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ProductController } from './interface/product.controller';
-import { ProductService } from './product.service';
+import { ProductController } from './presentation/product.controller';
+import { ProductService } from './application/product.service';
+import { PrismaModule } from '../../infrastructure/database/prisma.module';
+import { ProductRepository } from './infrastructure/product.repository';
+import { IProductRepository } from './domain/product-repository.interface';
+import { ProductDataMapper } from './infrastructure/product.data-mapper';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [
+    ProductService,
+    { provide: IProductRepository, useClass: ProductRepository },
+    ProductDataMapper,
+  ],
 })
 export class ProductModule {}
