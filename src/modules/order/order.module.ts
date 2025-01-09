c2428053ecf2a61c790fcd5221535a78a119bc14
jeong <jw.jeong@tilda.co.kr>
2025-01-10 00:00:22 +0900

    :sparkles: order module 구현

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
-import { OrderController } from './interface/order.controller';
-import { OrderService } from './order.service';
-
+import { OrderController } from './presentation/order.controller';
+import { OrderService } from './application/order.service';
+import { OrderFacade } from './application/order.facade';
+import { ProductModule } from '../product/product.module';
+import { PrismaModule } from '../../infrastructure/database/prisma.module';
+import { CustomerModule } from '../customer/customer.module';
+import { OrderDataMapper } from './infrastructure/order.data-mapper';
+import { OrderDetailDataMapper } from './infrastructure/order-detail.data-mapper';
+import { OrderRepository } from './infrastructure/order.repository';
+import { IOrderRepository } from './application/order-repository.interface';
 @Module({
+  imports: [ProductModule, PrismaModule, CustomerModule],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [
+    OrderService,
+    OrderFacade,
+    OrderDataMapper,
+    OrderDetailDataMapper,
+    {
+      provide: IOrderRepository,
+      useClass: OrderRepository,
+    },
+  ],
 })
 export class OrderModule {}
