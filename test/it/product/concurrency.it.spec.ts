e05d0fb76d97e6cf171c7cb21e97c63f681fa2b5
jeong <jw.jeong@tilda.co.kr>
2025-01-16 00:34:54 +0900

    :sparkles: 재고 통합 테스트 코드

--- test/it/product/concurrency.it.spec.ts
+++ test/it/product/concurrency.it.spec.ts
@@ -1,41 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaMockService } from '../prisma-service.mock';
+import { seedAll } from '../util/seed';
+import { ProductService } from '../../../src/modules/product/application/product.service';
+import { ClsModule } from 'nestjs-cls';
+import { AppModule } from '../../../src/app.module';
 import { PrismaService } from '../../../src/infrastructure/database/prisma.service';
-import { MockPrismaService } from '../prisma-service.mock';
-import { ProductRepository } from '../../../src/modules/product/infrastructure/product.repository';
-import { ProductDataMapper } from '../../../src/modules/product/infrastructure/product.data-mapper';
+import { clsModuleMockOption } from '../cls-module.mock';
+import { Product as ProductEntity } from '@prisma/client';
 
-describe('BalanceRepository 동시성 테스트', () => {
-  let productRepository: ProductRepository;
+describe('Production Service 동시성 테스트', () => {
+  let productService: ProductService;
   let prismaService: PrismaService;
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      providers: [
-        {
-          provide: PrismaService,
-          useClass: MockPrismaService,
-        },
-        ProductRepository,
-        ProductDataMapper,
-      ],
-    }).compile();
+      imports: [AppModule],
+    })
+      .overrideProvider(PrismaService)
+      .useClass(PrismaMockService)
+      .overrideModule(ClsModule)
+      .useModule(ClsModule.forRoot(clsModuleMockOption))
+      .compile();
 
-    productRepository = moduleRef.get<ProductRepository>(ProductRepository);
+    productService = moduleRef.get<ProductService>(ProductService);
     prismaService = moduleRef.get<PrismaService>(PrismaService);
   });
 
+  beforeEach(async () => {
+    await seedAll(prismaService);
+  });
+
   it('상품 재고 동시에 소모 했을 때 손실 없이 소모되는지 확인', async () => {
-    const productId = 1; // 테스트할 고객 ID
+    const productId = 1;
     const consumedStock = 2;
     const requests = Array.from({ length: 5 }, () =>
-      productRepository.consumeStock(productId, consumedStock),
+      productService.consumeStockList([
+        { id: productId, consumeStockAmount: consumedStock },
+      ]),
     );
 
-    await Promise.allSettled(requests);
+    const results = await Promise.allSettled(requests);
 
-    const savedProduct = await prismaService.product.findUnique({
-      where: { id: productId },
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        expect(result.value).toBeInstanceOf(Array);
+      }
     });
-    expect(savedProduct.stock).toBe(10 - consumedStock * 5);
+
+    const savedProduct = await prismaService.$queryRaw<
+      ProductEntity[]
+    >`SELECT * FROM hhpluscommerce.Product WHERE id = ${productId} for update`;
+
+    expect(savedProduct[0].stock).toBe(15 - consumedStock * 5);
   });
 });
