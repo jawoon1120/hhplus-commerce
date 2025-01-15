762eda5c7187cc9296907be93a267d6010a45227
jeong <jw.jeong@tilda.co.kr>
2025-01-15 23:08:33 +0900

    seed 생성

--- src/prisma/seed/customer.seed.ts
+++ src/prisma/seed/customer.seed.ts
@@ -1,29 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
 export const seedCustomer = async (prisma: PrismaClient) => {
-  const customer1 = await prisma.customer.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      name: '김항해',
-    },
+  const customers = await prisma.customer.createMany({
+    data: [
+      { id: 1, name: '김항해' },
+      { id: 2, name: '박항해' },
+      { id: 3, name: '이항해' },
+    ],
+    skipDuplicates: true,
   });
 
-  const customer2 = await prisma.customer.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
-      name: '박항해',
-    },
-  });
+  console.log(`Created ${customers.count} customer records`);
+};
 
-  const customer3 = await prisma.customer.upsert({
-    where: { id: 3 },
-    update: {},
-    create: {
-      name: '이항해',
-    },
-  });
-
-  console.log(customer1, customer2, customer3);
+export const truncateCustomer = async (prisma: PrismaClient) => {
+  await prisma.$executeRaw`TRUNCATE TABLE hhpluscommerce.Customer`;
 };
