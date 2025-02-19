5633a5440e1ac741506c71b095b5bad74b8aeed0
jeong <jw.jeong@tilda.co.kr>
2025-02-19 23:22:58 +0900

    테스트 프로듀서 삭제

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,43 +1,14 @@
-import {
-  Controller,
-  Get,
-  Inject,
-  OnModuleDestroy,
-  OnModuleInit,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
-import { ClientKafka } from '@nestjs/microservices';
 
 @Controller()
-export class AppController implements OnModuleInit, OnModuleDestroy {
-  constructor(
-    private readonly appService: AppService,
-    @Inject('KAFKA_CLIENT') private readonly kafkaClient: ClientKafka,
-  ) {}
+export class AppController {
+  constructor(private readonly appService: AppService) {}
 
   @ApiExcludeEndpoint()
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Post('kafka')
-  getKafka(): string {
-    this.kafkaClient.emit('TEST', {
-      message: 'Hello World',
-    });
-    return 'Hello World';
-  }
-
-  async onModuleInit(): Promise<void> {
-    const topic = ['TEST'];
-    topic.forEach((t) => this.kafkaClient.subscribeToResponseOf(t));
-    await this.kafkaClient.connect();
-  }
-
-  async onModuleDestroy(): Promise<void> {
-    await this.kafkaClient.close();
   }
 }
