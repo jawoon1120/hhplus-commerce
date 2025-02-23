184145f63c998e40a23419834792f37750248799
jeong <jw.jeong@tilda.co.kr>
2025-02-23 22:27:04 +0900

    fix: 각 다른 유저가 동시에 포인트 충전

--- load-test/charge-balance.js
+++ load-test/charge-balance.js
@@ -4,12 +4,6 @@ import http from 'k6/http';
 
 export let options = {
   scenarios: {
-    // burst_test: {
-    //   executor: 'shared-iterations',
-    //   vus: 100,
-    //   iterations: 100,
-    //   maxDuration: '1s',
-    // },
     constant_load: {
       executor: 'constant-vus',
       vus: 20,
@@ -18,13 +12,40 @@ export let options = {
   },
 };
 
+// VU별 시작 customerId 설정
+const getCustomerIdRange = (vuId) => {
+  const startCustomerId = 1 + (vuId - 1) * 10; // VU 1은 10000011부터, VU 2는 10000021부터...
+  return startCustomerId;
+};
+
+let vuCustomerIds = new Map();
+
 export default function () {
-  let res = http.post('http://localhost:3000/users/1/balances', {
-    customerId: 1,
+  if (!vuCustomerIds.has(__VU)) {
+    vuCustomerIds.set(__VU, getCustomerIdRange(__VU));
+  }
+
+  let currentCustomerId = vuCustomerIds.get(__VU);
+  vuCustomerIds.set(__VU, currentCustomerId + 1);
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({
+    customerId: currentCustomerId,
     amount: 100,
-  }); // Update with your API endpoint
+  });
+  console.log(currentCustomerId);
+  let res = http.post(
+    `http://localhost:3000/users/${currentCustomerId}/balances`,
+    body,
+    params,
+  );
   check(res, {
-    'status is 201 or 404': (r) => r.status === 201 || r.status === 404,
+    'status is 201': (r) => r.status === 201,
   });
   sleep(1);
 }
--- src/modules/balance/application/balance-repository.interface.ts
+++ src/modules/balance/application/balance-repository.interface.ts
@@ -2,6 +2,7 @@ import { Balance } from '../domain/balance.domain';
 
 export interface IBalanceRepository {
   getBalanceByUserId(customerId: number): Promise<Balance>;
+  createBalance(balance: Balance): Promise<Balance>;
   chargeBalance(balance: Balance): Promise<Balance>;
   withdrawBalance(balance: Balance): Promise<Balance>;
   getBalanceByUserIdWithLock(customerId: number): Promise<Balance>;
